8da05b73071e2f7f791d563c5f61a6de58ea8e16
Eddie Cantu <eddiecantu@gmail.com>
2011-12-03 16:33:27 -0600

    more stuffs

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,4 +1,4 @@
-$( '#checklist' ).on( 'pageinit',function(event,data){
+$( '#checklist' ).live( 'pageinit',function(event,data){
     var theList = doT.template($("#checklist-list").html());
   $.get("someurl",function(res){
       $("#checklist-list").html(theList(res));
