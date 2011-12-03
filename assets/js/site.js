40a499cfd302787e3bb1f9b0fbb3439edabbed3c
Eddie Cantu <eddiecantu@gmail.com>
2011-12-03 15:44:22 -0600

    fixing my last commit

---
+++ assets/js/site.js
@@ -0,0 +1,13 @@
+$( '#checklist' ).on( 'pageinit',function(event,data){
+    var theList = doT.template($("#checklist-list").html());
+  $.get("someurl",function(res){
+      $("#checklist-list").html(theList(res));
+  })
+});
+
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
