53ee457cf1fe3b82b3aaa81319c5b5b1239d5785
Eddie Cantu <eddiecantu@gmail.com>
2011-12-03 23:53:22 -0600

    aded nav bar and google maps integration

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,3 +4,18 @@ $( '#checklist' ).live( 'pageinit',function(event,data){
       $("#checklist-list").html(theList(res));
   })
 });
+
+$( '#home' ).live( 'pageinit',function(event,data){
+    var theList = doT.template($("#random-fact").html());
+  $.get("someurl",function(res){
+      $("#random-fact").html(theList(res));
+  })
+});
+
+$( '#ready' ).live( 'pageinit',function(event,data){
+    var theList = doT.template($("#ready-list").html());
+  $.get("someurl",function(res){
+      $("#ready-list").html(theList(res));
+  })
+});
+
